415aa676532143ba55bacfe781a454c703fef3ae
danxiaonuo <danxiaonuo@danxiaonuo.me>
2023-04-03 10:24:43 +0800

    Update system.ts

--- src/system.ts
+++ src/system.ts
@@ -11,6 +11,7 @@ export const defaultSetting = {
 }
 
 export const defaultMessage = `Powered by OpenAI Vercel
+- 本站由 [xiaonuo](https://www.danxiaonuo.me/) 提供技术支持。
 - [[Shift]] + [[Enter]] 换行。开头输入 [[/]] 或者 [[空格]] 搜索 Prompt 预设。[[↑]] 可编辑最近一次提问。点击顶部名称滚动到顶部，点击输入框滚动到底部。`
 
 export type Setting = typeof defaultSetting
